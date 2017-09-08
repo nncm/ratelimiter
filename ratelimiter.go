69de97f6ca3d74d4a647dd3b58b92e0b23e63f7d
wangtaodobbin <wangtaodobbin@didichuxing.com>
2017-09-08 17:08:16 +0800

    add testing

--- ratelimiter.go
+++ ratelimiter.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-type RateLimiter struct {
+type ratelimiter struct {
 	rate          float64
 	interval      float64
 	maxPermits    float64
@@ -16,12 +16,12 @@ type RateLimiter struct {
 	mut           sync.Mutex
 }
 
-func NewRateLimiter() *RateLimiter {
-	m := &RateLimiter{}
+func NewRateLimiter() *ratelimiter {
+	m := &ratelimiter{}
 	return m
 }
 
-func (m *RateLimiter) SetRate(rate float64) error {
+func (m *ratelimiter) SetRate(rate float64) error {
 	if rate <= 0.0 {
 		return errors.New("RateLimiter: Rate must be greater than 0")
 	}
@@ -37,11 +37,11 @@ func (m *RateLimiter) SetRate(rate float64) error {
 	return nil
 }
 
-func (m *RateLimiter) GetRate() float64 {
+func (m *ratelimiter) GetRate() float64 {
 	return m.rate
 }
 
-func (m *RateLimiter) Aquire(permits float64) error {
+func (m *ratelimiter) Aquire(permits float64) error {
 	if permits < 0 || permits > m.maxPermits {
 		return errors.New("Ratelimite: Permits must be greater than 0, and smaller than maxPermit")
 	}
@@ -59,7 +59,7 @@ func (m *RateLimiter) Aquire(permits float64) error {
 	return nil
 }
 
-func (m *RateLimiter) TryAcquire(permits float64, timeout int64) bool {
+func (m *ratelimiter) TryAcquire(permits float64, timeout int64) bool {
 	if permits < 0 || permits > m.maxPermits {
 		return false
 	}
@@ -81,22 +81,22 @@ func (m *RateLimiter) TryAcquire(permits float64, timeout int64) bool {
 	return true
 }
 
-func (m *RateLimiter) futurePermits(future int64) float64 {
+func (m *ratelimiter) futurePermits(future int64) float64 {
 	return float64(future-m.nextFree) / m.interval
 }
 
-func (m *RateLimiter) sync(now int64) {
+func (m *ratelimiter) sync(now int64) {
 	if now > m.nextFree {
 		m.storedPermits = math.Min(m.maxPermits, m.storedPermits+m.futurePermits(now))
 		m.nextFree = now
 	}
 }
 
-func (m *RateLimiter) nowMicroSecond() int64 {
+func (m *ratelimiter) nowMicroSecond() int64 {
 	return time.Now().UnixNano() / 1000
 }
 
-func (m *RateLimiter) claimNext(permits float64, now int64) int64 {
+func (m *ratelimiter) claimNext(permits float64, now int64) int64 {
 	m.sync(now)
 
 	stored := math.Min(permits, m.storedPermits)
